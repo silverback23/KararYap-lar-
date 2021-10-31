b5b025a6ef22c0153812fd180d550b1047a75f1e
silverback23 <cahid23@hotmail.com>
2021-10-31 12:40:32 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace kararYapıları
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time =DateTime.Now.Hour;
+            if(time>=6&&time<=11)
+                Console.WriteLine("Günaydın");
+            else if(time>=18)
+                Console.WriteLine("İyi günler");
+             else
+                Console.WriteLine("İyi Geceler");
+
+            string sonuc=time<=18 ? "iyi günler" :"iyi geceler?";
+            
+        }
+    }
+}
